386c965663e8c1fe03b4028307398cf8257f7203
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2022-04-21 17:58:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
-function preload(){}
-function setup(){
-    canvas=createCanvas(400,400);
-    canvas.center();
+song1="";
+song2="";
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
 }
-function draw(){}
+
+function setup(){
+    canvas=createCanvas(600,500);
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function play(){
+    song1.play();
+}

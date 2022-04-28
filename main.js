98f20314bbdf833d5b2bbec406508b8d7f74b41d
Chandan0424 <76838260+Chandan0424@users.noreply.github.com>
2022-04-28 17:35:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload(){
-    song1=loadSound("music.mp3");
-    song2=loadSound("music2.mp3");
+    song1=loadSound("music.mp3")
+    song2=loadSound("music2.mp3")
 }
 
 function setup(){
     canvas=createCanvas(600,500);
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Loaded")
 }
 
 function draw(){
@@ -18,4 +28,22 @@ function draw(){
 
 function play(){
     song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
+    song1.stop();
+    song2.play();
+    song2.setVolume(1);
+    song2.rate(1);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX= " + leftWristX +" leftWristY= " + leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX= "+rightWristX+" rightWristy= "+rightWristY);
+    }
 }
